82e29834751c0ed7c54ca64fcd5e109625bdce44
unknown <shrabastibirthday@gmail.com>
2026-06-06 10:55:33 +0530

    finished first scrapbook section

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function openScrapbook() {
 
+    // PLAY MUSIC
+    const music = document.getElementById("bg-music");
+
+    music.play();
+
+    // SCROLL
     document.querySelector(".landing")
     .scrollIntoView({
         behavior: "smooth"
